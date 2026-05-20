312d7abbeb4d524cb97c8d9f4b401f6afaae3e39
Paradise Nursery <noreply@paradisenursery.local>
2026-05-20 19:47:07 +0200

    Add cart-item-delete class and show total cart amount in CartItem

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,11 +1,22 @@
-import { useDispatch } from 'react-redux'
-import { updateQuantity, removeItem } from '../redux/CartSlice.jsx'
+import { useDispatch, useSelector } from 'react-redux'
+import { updateQuantity, removeItem, selectCartItems } from '../redux/CartSlice.jsx'
 
 function CartItem({ item }) {
   const dispatch = useDispatch()
+  const cartItems = useSelector(selectCartItems)
+  const showCartTotal = cartItems.length > 0 && cartItems[0].id === item.id
+
+  const calculateTotalCartAmount = (items) =>
+    items.reduce((sum, current) => sum + current.price * current.quantity, 0)
 
   return (
     <article className="cart-item">
+      {showCartTotal && (
+        <div className="cart-summary-banner">
+          <p className="cart-total-label">Cart total amount</p>
+          <p className="cart-total-value">${calculateTotalCartAmount(cartItems).toFixed(2)}</p>
+        </div>
+      )}
       <img className="cart-item-image" src={item.image} alt={item.name} />
       <div className="cart-item-details">
         <h3>{item.name}</h3>
@@ -22,7 +33,11 @@ function CartItem({ item }) {
             +
           </button>
         </div>
-        <button type="button" className="button button-secondary delete-button" onClick={() => dispatch(removeItem(item.id))}>
+        <button
+          type="button"
+          className="button button-secondary delete-button cart-item-delete"
+          onClick={() => dispatch(removeItem(item.id))}
+        >
           Delete
         </button>
       </div>
